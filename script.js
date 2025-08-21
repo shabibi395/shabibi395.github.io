15b485f3a04123ba13ce14a7a0479738e1aa964b
Seza Habibi <dbcodeexplorer@gmail.com>
2025-08-21 11:34:01 -0700

    version 1.0.0

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+if (toggleButton && navLinks) {
+    toggleButton.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+}
